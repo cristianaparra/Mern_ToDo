fac68c5663fae5d310017848de45f18127e4cfb8
Cristian Parra <cristian.a.parra@gmail.com>
2020-09-15 12:28:24 -0300

    Creando barra para saludo de usuario y boton cerras sesion

--- src/components/proyectos/Proyectos.js
+++ src/components/proyectos/Proyectos.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Sidebar from '../layout/Sidebar'
+import Barra from '../layout/Barra'
 
 const Proyectos = () => {
     return (
         <div className='contenedor-app'>
-          <Sidebar />
+            <Sidebar />
             <div className="seccion-principal">
-
+                <Barra />
                 <main>
 
                     <div className="contenedor-tareas">
