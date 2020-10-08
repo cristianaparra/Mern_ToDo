fdb89fdba835e7876686b2fa8d0bc8b09b59c39e
Cristian Parra <cristian.a.parra@gmail.com>
2020-10-08 15:37:25 -0300

    creando el satet de alertas

--- src/App.js
+++ src/App.js
@@ -3,24 +3,28 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './components/auth/Login'
 import NuevaCuenta from './components/auth/NuevaCuenta'
 import Proyectos from './components/proyectos/Proyectos'
+
 import ProyectoState from './context/proyectos/proyectoState'
 import TareaState from './context/tareas/tareaState'
+import AlertaState from './context/alertas/alertaState'
 
 
 function App() {
   return (
     <ProyectoState>
-      <TareaState>
+      <AlertaState>
+        <TareaState>
 
-        <Router>
-          <Switch>
-            <Route exact path='/' component={Login} />
-            <Route exact path='/nueva-cuenta' component={NuevaCuenta} />
-            <Route exact path='/proyectos' component={Proyectos} />
-          </Switch>
-        </Router>
+          <Router>
+            <Switch>
+              <Route exact path='/' component={Login} />
+              <Route exact path='/nueva-cuenta' component={NuevaCuenta} />
+              <Route exact path='/proyectos' component={Proyectos} />
+            </Switch>
+          </Router>
 
-      </TareaState>
+        </TareaState>
+      </AlertaState>
     </ProyectoState>
   );
 }
